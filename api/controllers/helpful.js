7006c6dc867ae4c41f7959cc56d835649f5bcf4b
Steven Klier <steven.klier@zenotech.io>
2021-08-18 18:39:51 -0500

    slight response change for helpful endpoint

--- api/controllers/helpful.js
+++ api/controllers/helpful.js
@@ -6,7 +6,7 @@ exports.helpful = async (req, res) => {
         const getHelpfulness = await pool.query(`SELECT (helpfulness) FROM reviews WHERE id=${review_id}`)
         let currentHelpfulness = getHelpfulness.rows[0].helpfulness
         const updateHelpfulness = await pool.query(`UPDATE reviews SET helpfulness=${currentHelpfulness + 1} WHERE id=${review_id} RETURNING *`)
-        res.send(updateHelpfulness.statusCode)
+        res.send('Review now marked as helpful.')
     } catch(err) {
         console.log(err);
     }
